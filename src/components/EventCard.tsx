38f8e00542684529b77eab6266e1959144a43861
LipT <feelipeavencourt@gmail.com>
2025-10-11 13:04:29 -0300

    Estilizado página criar evento - UC9

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,6 +1,7 @@
 // src/components/EventCard.tsx
 import { Evento } from '@/types';
 import Link from 'next/link';
+import styles from './Card_evento.module.css';
 
 interface Props {
   evento: Evento;
@@ -8,8 +9,8 @@ interface Props {
 
 export const EventCard = ({ evento }: Props) => {
   return (
-    <div className="border p-4 rounded-lg shadow hover:shadow-md transition-shadow">
-      <h2 className="text-xl font-bold">{evento.nome}</h2>
+    <div className={`${styles['Card_event']} border p-4 rounded-lg shadow hover:shadow-md transition-shadow`}>
+      <h2 className={`${styles['Title']} text-xl font-bold`}>{evento.nome}</h2>
       <p className="text-gray-600">{new Date(evento.data).toLocaleDateString()}</p>
       <p className="text-gray-500">{evento.local}</p>
       <Link href={`/events/${evento.id}`} className="text-indigo-600 hover:underline mt-2 inline-block">
